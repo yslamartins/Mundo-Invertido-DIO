6e0c59f7a172fca899616e5b820dd369ef8b2a87
Ysla Martins <91572105+yslamartins@users.noreply.github.com>
2022-09-06 15:59:40 -0300

    Integração-Banco-de-Dados

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,21 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVouAhqkkNxdM8Pk0qKqRaoAOykxizGlk",
+  authDomain: "clube-de-dd.firebaseapp.com",
+  projectId: "clube-de-dd",
+  storageBucket: "clube-de-dd.appspot.com",
+  messagingSenderId: "943525548026",
+  appId: "1:943525548026:web:3bfc712e129355f7cade43"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+  export default app 
---
+++ assets/js/firebase/hellfire-club.js
@@ -0,0 +1,10 @@
+
+import app from "./app.js"
+import {getFirestore, collection, addDoc} from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js'
+
+export async function subscribeToHellfireClub(subscription) { 
+  const db = getFirestore(app)
+  const hellfireClubCollection = collection(db,"hellfire-clube")
+  const docRef = await addDoc(hellfireClubCollection, subscription)
+  return docRef.id
+}
